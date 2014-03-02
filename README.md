863dbf11f856f87c44bbb640c6ad99017961b705
dokechin <user@user-PC.(none)>
2014-03-03 04:42:10 +0900

    Checking in changes prior to tagging of version 0.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 11de9b1..df300a2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,5 +2,7 @@ Revision history for Perl extension Mojolicious-Plugin-BootstrapPagination
    
     {{$NEXT}}
    
    +0.01 2014-03-02T19:41:49Z
    +
         - original version

--- README.md
+++ README.md
@@ -1,14 +1,54 @@
 # NAME
 
-Mojolicious::Plugin::BootstrapPagination - It's new $module
+Mojolicious::Plugin::PageNavigator - Page Navigator plugin for Mojolicious
 
 # SYNOPSIS
 
-    use Mojolicious::Plugin::BootstrapPagination;
+    # Mojolicious::Lite
+    plugin 'bootstrap_nagination'
+
+    # Mojolicious
+    $self->plugin( 'bootstrap_nagination' );
 
 # DESCRIPTION
 
-Mojolicious::Plugin::BootstrapPagination is ...
+[Mojolicious::Plugin::PageNavigator](http://search.cpan.org/perldoc?Mojolicious::Plugin::PageNavigator) generates standard page navigation bar, like 
+  
+
+<<  1  2 ... 11 12 13 14 15 ... 85 86 >>
+
+# HELPERS
+
+## bootstrap\_pagination
+
+    %= bootstrap_nagination( $current_page, $total_pages, $opts );
+
+### Options
+
+Options is a optional ref hash.
+
+    %= bootstrap_nagination( $current_page, $total_pages, {
+        round => 4,
+        outer => 2,
+        param => 'page' } );
+
+- round
+
+    Number of pages arround the current page. Default: 4.
+
+- outer
+
+    Number of outer window pages (first and last pages). Default 2.
+
+- param
+
+    Name of param for query url. Default: 'page'
+
+# SEE ALSO
+
+[Mojolicious](http://search.cpan.org/perldoc?Mojolicious), [Mojolicious::Guides](http://search.cpan.org/perldoc?Mojolicious::Guides), [http://mojolicio.us](http://mojolicio.us),[Mojolicious::Plugin::PageNavigator](http://search.cpan.org/perldoc?Mojolicious::Plugin::PageNavigator).
+
+
 
 # LICENSE
 
--- lib/Mojolicious/Plugin/BootstrapPagination.pm
+++ lib/Mojolicious/Plugin/BootstrapPagination.pm
@@ -1,11 +1,69 @@
 package Mojolicious::Plugin::BootstrapPagination;
-use 5.008005;
+use Mojo::Base 'Mojolicious::Plugin';
+use POSIX( qw/ceil/ );
+use Mojo::ByteStream 'b';
+
 use strict;
 use warnings;
 
 our $VERSION = "0.01";
 
+# Homer: Well basically, I just copied the plant we have now.
+#        Then, I added some fins to lower wind resistance.  
+#        And this racing stripe here I feel is pretty sharp.
+# Burns: Agreed.  First prize!
+sub  register{
+  my ( $self, $app, $args ) = @_;
+  $args ||= {};
 
+  $app->helper( bootstrap_nagination => sub{
+      my ( $self, $actual, $count, $opts ) = @_;
+      $count = ceil($count);
+      return "" unless $count > 1;
+      $opts = {} unless $opts;
+      my $round = $opts->{round} || 4;
+      my $param = $opts->{param} || "page";
+      my $outer = $opts->{outer} || 2;
+      my @current = ( $actual - $round .. $actual + $round );
+      my @first   = ( $round > $actual ? (1..$round * 3 ) : (1..$outer) );
+      my @tail    = ( $count - $round < $actual ? ( $count - $round * 2 + 1 .. $count ) : 
+          ( $count - $outer + 1 .. $count ) );
+      my @ret = ();
+      my $last = undef;
+      foreach my $number( sort { $a <=> $b } @current, @first, @tail ){
+        next if $last && $last == $number;
+        next if $number <= 0 ;
+        last if $number > $count;
+        push @ret, ".." if( $last && $last + 1 != $number );
+        push @ret, $number;
+        $last = $number;
+      }
+      
+      my $html = "<ul class=\"pagination\">";
+      if( $actual == 1 ){
+        $html .= "<li class=\"disabled\"><a href=\"#\" >&laquo;</a></li>";
+      } else {
+        $html .= "<li><a href=\"" . $self->url_for->query( $param => $actual - 1 ) . "\" >&laquo;</a></li>";
+      }
+      foreach my $number( @ret ){
+        if( $number eq ".." ){
+          $html .= "<li><a href=\"#\" >..</a></li>";
+        } elsif( $number == $actual ) {
+        $html .= "<li class=\"disabled\"><a href=\"#\">$number</a></li>";
+        } else {
+          $html .= "<li><a href=\"" . $self->url_for->query( $param => $number ) ."\">$number</a></li>";
+        }
+      }
+      if( $actual == $count ){
+        $html .= "<li class=\"disabled\"><a href=\"" . $self->url_for->query( $param => $actual + 1 ) . "\" >&laquo;</a></li>";
+      } else {
+        $html .= "<li><a href=\"" . $self->url_for->query( $param => $actual + 1 ) . "\" >&raquo;</a></li>";
+      }
+      $html .= "</ul>";
+      return b( $html );
+    } );
+
+}
 
 1;
 __END__
@@ -14,15 +72,57 @@ __END__
 
 =head1 NAME
 
-Mojolicious::Plugin::BootstrapPagination - It's new $module
+Mojolicious::Plugin::PageNavigator - Page Navigator plugin for Mojolicious
 
 =head1 SYNOPSIS
 
-    use Mojolicious::Plugin::BootstrapPagination;
+  # Mojolicious::Lite
+  plugin 'bootstrap_nagination'
+
+  # Mojolicious
+  $self->plugin( 'bootstrap_nagination' );
 
 =head1 DESCRIPTION
 
-Mojolicious::Plugin::BootstrapPagination is ...
+L<Mojolicious::Plugin::PageNavigator> generates standard page navigation bar, like 
+  
+<<  1  2 ... 11 12 13 14 15 ... 85 86 >>
+
+=head1 HELPERS
+
+=head2 bootstrap_pagination
+
+  %= bootstrap_nagination( $current_page, $total_pages, $opts );
+
+=head3 Options
+
+Options is a optional ref hash.
+
+  %= bootstrap_nagination( $current_page, $total_pages, {
+      round => 4,
+      outer => 2,
+      param => 'page' } );
+
+=over 1
+
+=item round
+
+Number of pages arround the current page. Default: 4.
+
+=item outer
+
+Number of outer window pages (first and last pages). Default 2.
+
+=item param
+
+Name of param for query url. Default: 'page'
+
+=back
+
+=head1 SEE ALSO
+
+L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicio.us>,L<Mojolicious::Plugin::PageNavigator>.
+
 
 =head1 LICENSE
 
