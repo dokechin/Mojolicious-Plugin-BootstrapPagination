de48ec93a851edaae91c048498159bea6bcb4576
dokechin <user@user-PC.(none)>
2014-03-03 04:40:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojolicious::Plugin::BootstrapPagination - It's new $module
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::BootstrapPagination;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::BootstrapPagination is ...
+
+# LICENSE
+
+Copyright (C) dokechin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+dokechin <>
---
+++ lib/Mojolicious/Plugin/BootstrapPagination.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::BootstrapPagination;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::BootstrapPagination - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::BootstrapPagination;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::BootstrapPagination is ...
+
+=head1 LICENSE
+
+Copyright (C) dokechin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+dokechin E<lt>E<gt>
+
+=cut
+
