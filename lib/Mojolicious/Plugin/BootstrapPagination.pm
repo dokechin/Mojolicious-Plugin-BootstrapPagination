8f0ef0ec25c7b0a83237485dfa446eeb755026cd
Andrey Kuzmin <chips@reg.ru>
2014-08-06 04:59:18 +0400

    Add additional optional parametres 'start' and 'query'

--- lib/Mojolicious/Plugin/BootstrapPagination.pm
+++ lib/Mojolicious/Plugin/BootstrapPagination.pm
@@ -1,12 +1,12 @@
 package Mojolicious::Plugin::BootstrapPagination;
 use Mojo::Base 'Mojolicious::Plugin';
-use POSIX( qw/ceil/ );
+use POSIX( qw/ceil floor/ );
 use Mojo::ByteStream 'b';
 
 use strict;
 use warnings;
 
-our $VERSION = "0.09";
+our $VERSION = "0.10";
 
 # Homer: Well basically, I just copied the plant we have now.
 #        Then, I added some fins to lower wind resistance.  
@@ -23,45 +23,56 @@ sub  register{
       $opts = {} unless $opts;
       my $round = $opts->{round} || 4;
       my $param = $opts->{param} || "page";
-      my $class = $opts->{class}  || "";
+      my $class = $opts->{class} || "";
       if ($class ne ""){
           $class = " " . $class;
       }
       my $outer = $opts->{outer} || 2;
+      my $query = $opts->{query} || "";
+      my $start = $opts->{start} // 1;
       my @current = ( $actual - $round .. $actual + $round );
-      my @first   = ( $round > $actual ? (1..$round * 3 ) : (1..$outer) );
+      my @first   = ( $round > $actual ? ($start..$round * 3 ) : ($start..$outer) );
       my @tail    = ( $count - $round < $actual ? ( $count - $round * 2 + 1 .. $count ) : 
           ( $count - $outer + 1 .. $count ) );
       my @ret = ();
       my $last = undef;
       foreach my $number( sort { $a <=> $b } @current, @first, @tail ){
-        next if $last && $last == $number;
-        next if $number <= 0 ;
-        last if $number > $count;
+        next if ( $last && $last == $number && $start > 0 ) || ( defined $last && $last == $number && $start == 0 );
+        next if ( $number <= 0 && $start > 0) || ( $number < 0 && $start == 0 );
+        last if ( $number > $count && $start > 0 ) || ( $number >= $count && $start == 0 );
         push @ret, ".." if( $last && $last + 1 != $number );
         push @ret, $number;
         $last = $number;
       }
-      
       my $html = "<ul class=\"pagination$class\">";
-      if( $actual == 1 ){
+      if( $actual == $start ){
         $html .= "<li class=\"disabled\"><a href=\"#\" >&laquo;</a></li>";
       } else {
-        $html .= "<li><a href=\"" . $self->url_for->query( $param => $actual - 1 ) . "\" >&laquo;</a></li>";
+        $html .= "<li><a href=\"" . $self->url_for->query( $param => $actual - 1 ) . $query . "\" >&laquo;</a></li>";
       }
+      my $last_num = -1;
       foreach my $number( @ret ){
-        if( $number eq ".." ){
-          $html .= "<li><a href=\"#\" >..</a></li>";
-        } elsif( $number == $actual ) {
-        $html .= "<li class=\"disabled\"><a href=\"#\">$number</a></li>";
-        } else {
-          $html .= "<li><a href=\"" . $self->url_for->query( $param => $number ) ."\">$number</a></li>";
+        my $show_number = $start > 0 ? $number : ( $number =~ /\d+/ ? $number + 1 : $number );
+        if( $number eq ".." && $last_num < $actual ){
+          my $offset = ceil( ( $actual - $round ) / 2 ) + 1 ;
+          $html .= "<li><a href=\"" . $self->url_for->query( $param => $start == 0 ? $offset + 1 : $offset ) . $query ."\" >..</a></li>";
         }
+        elsif( $number eq ".." && $last_num > $actual ) {
+          my $back = $count - $outer + 1;
+          my $forw = $round + $actual;
+          my $offset = ceil( ( ( $back - $forw ) / 2 ) + $forw );
+          $html .= "<li><a href=\"" . $self->url_for->query( $param => $start == 0 ? $offset + 1 : $offset ) . $query ."\" >..</a></li>";
+        } elsif( $number == $actual ) {
+          $html .= "<li class=\"disabled\"><a href=\"#\">$show_number</a></li>";
+        } else {
+          $html .= "<li><a href=\"" . $self->url_for->query( $param => $number ) . $query ."\">$show_number</a></li>";
+        }
+         $last_num = $number;
       }
       if( $actual == $count ){
-        $html .= "<li class=\"disabled\"><a href=\"" . $self->url_for->query( $param => $actual + 1 ) . "\" >&laquo;</a></li>";
+        $html .= "<li class=\"disabled\"><a href=\"" . $self->url_for->query( $param => $actual + 1 ) . $query . "\" >&raquo;</a></li>";
       } else {
-        $html .= "<li><a href=\"" . $self->url_for->query( $param => $actual + 1 ) . "\" >&raquo;</a></li>";
+        $html .= "<li><a href=\"" . $self->url_for->query( $param => $actual + 1 ) . $query . "\" >&raquo;</a></li>";
       }
       $html .= "</ul>";
       return b( $html );
@@ -106,6 +117,8 @@ Options is a optional ref hash.
   %= bootstrap_pagination( $current_page, $total_pages, {
       round => 4,
       outer => 2,
+      query => "&id=$id",
+      start => 1,
       class => 'pagination-lg',
       param => 'page' } );
 
@@ -122,6 +135,14 @@ Number of outer window pages (first and last pages). Default 2.
 =item param
 
 Name of param for query url. Default: 'page'
+
+=item query
+
+Additional query string to urls. Optional.
+
+=item start
+
+Start number for query string. Default: 1. Optional.
 
 =back
 
@@ -143,6 +164,10 @@ it under the same terms as Perl itself.
 =head1 AUTHOR
 
 dokechin E<lt>E<gt>
+
+=head1 CONTRIBUTORS
+
+Andrey Chips Kuzmin <chipsoid@cpan.org>
 
 =cut
 
