f0ad3308814b105dddd59f908ee4df6319c056d0
Alexander Groshev <sattellite@yandex.ru>
2014-10-21 12:10:48 +0400

    Changed markup for active page in Bootstrap 3
    Active page must not be as link. It must be in `<span>` tag and parent `<li>` tag must have class `active.`
    Example [here](http://getbootstrap.com/components/#pagination-default).

--- lib/Mojolicious/Plugin/BootstrapPagination.pm
+++ lib/Mojolicious/Plugin/BootstrapPagination.pm
@@ -62,7 +62,7 @@ sub  register{
           my $offset = ceil( ( ( $back - $forw ) / 2 ) + $forw );
           $html .= "<li><a href=\"" . $self->url_for->query( $param => $start == 0 ? $offset + 1 : $offset ) . $query ."\" >..</a></li>";
         } elsif( $number == $actual ) {
-          $html .= "<li class=\"disabled\"><a href=\"#\">$show_number</a></li>";
+          $html .= "<li class=\"active\"><span>$show_number</span></li>";
         } else {
           $html .= "<li><a href=\"" . $self->url_for->query( $param => $number ) . $query ."\">$show_number</a></li>";
         }
